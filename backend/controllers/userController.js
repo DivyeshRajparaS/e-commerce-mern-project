a44128c62fceb252db17a0556bab9423f46c4782
DivyeshRajparaS <divyeshrajpara23@gmail.com>
2025-04-17 16:13:12 +0530

    fix: update logoutUser response message for clarity

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -130,7 +130,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     return res.status(200)
         .clearCookie("accessToken", options)
         .clearCookie("refreshToken", options)
-        .json(new ApiResponse(200, {}, "User logged out!!"))
+        .json({ success: true, message: "Logout sucessfully" })
 })
 
 const adminLogin = async (req, res) => {
