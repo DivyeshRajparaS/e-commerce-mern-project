82d0c1b01863244769ab7db02039dc6be67659a8
DivyeshRajparaS <divyeshrajpara23@gmail.com>
2025-04-17 16:10:08 +0530

    adding JWTverify in logoutuser

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,7 +6,7 @@ const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
-userRouter.post('/logout', logoutUser);
+userRouter.post('/logout', verifyJWT, logoutUser);
 userRouter.post('/admin', adminLogin);
 userRouter.post('/showdata', verifyJWT, showData);
 userRouter.post('/updateuser/username', verifyJWT, changeUsername);
